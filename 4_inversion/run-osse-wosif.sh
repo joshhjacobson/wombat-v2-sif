8e395ddfe20f5cbe3a4a1da988a1592bb6454318
joshhjacobson <joshuah.jacobson@gmail.com>
2024-06-06 14:27:32 +1000

    Tidy git tracked files

--- 4_inversion/run-osse-wosif.sh
+++ 4_inversion/run-osse-wosif.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHA0-WOSIF.rds
+OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHASMALL-FIXRESP-WOSIF.rds
 sleeps 5s
 
-OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHAV2-WOSIF.rds
+OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHASMALL-FREERESP-WOSIF.rds
 sleep 5s
 
-OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHAFREE-WOSIF.rds
+OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHALARGE-FIXRESP-WOSIF.rds
+sleep 5s
+
+OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHALARGE-FREERESP-WOSIF.rds
 sleep 5s

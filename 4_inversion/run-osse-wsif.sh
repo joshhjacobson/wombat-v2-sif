61f7ab32b99c1b7459299a413b8a7326b185a9cc
joshhjacobson <joshuah.jacobson@gmail.com>
2024-07-19 09:12:43 +1000

    Minor tweaks

--- 4_inversion/run-osse-wsif.sh
+++ 4_inversion/run-osse-wsif.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHASMALL-FIXRESP-WSIF.rds
-sleeps 5s
-
 OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHASMALL-FREERESP-WSIF.rds
 sleep 5s
 
-OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHALARGE-FIXRESP-WSIF.rds
+OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHASMALL-FIXRESP-WSIF.rds
 sleep 5s
 
-OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHALARGE-FREERESP-WSIF.rds
+OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHA0-FIXRESP-WSIF.rds
+sleep 5s
+
+OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHA0-FREERESP-WSIF.rds
+sleep 5s
+
+OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHAV2-FIXRESP-WSIF.rds
+sleep 5s
+
+OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHAV2-FREERESP-WSIF.rds
+sleep 5s
+
+OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHAMD-FIXRESP-WSIF.rds
+sleep 5s
+
+OMP_NUM_THREADS=16 WOMBAT_LOG_LEVEL=trace make 4_inversion/intermediates/osse-samples-ALPHAMD-FREERESP-WSIF.rds
 sleep 5s

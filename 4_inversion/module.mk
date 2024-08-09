12ae99de8de7b7c9c04749faf2243bed52d33347
joshhjacobson <joshuah.jacobson@gmail.com>
2024-08-09 13:07:23 +1000

    Revert from long-run sample size

--- 4_inversion/module.mk
+++ 4_inversion/module.mk
@@ -132,8 +132,8 @@ $(OSSE_SAMPLES_BASE)-%-WSIF.rds: \
 	$(H_IS) \
 	$(H_SIF)
 	Rscript $< $(OSSE_FLAGS) \
-		--n-samples 2500 \
-		--n-warm-up 500 \
+		--n-samples 200 \
+		--n-warm-up 100 \
 		--observations $(OSSE_OBSERVATIONS_BASE)-$(firstword $(subst -, ,$*)).fst \
 		--basis-vectors $(BASIS_VECTORS) \
 		--prior $(PRIOR) \
